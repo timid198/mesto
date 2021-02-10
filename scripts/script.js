a61ed72fc36f947ae9f87190123f8e70298a7b47
Andrey Azovkin <adm.az-an@yandex.ru>
2021-02-10 19:52:50 +0300

    пятый спринт, отправляю на проверку #1

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,8 @@
+// добавление карточек на страницу
 const templateEl = document.querySelector('.template__element');
 const listContainerEl = document.querySelector('.elements');
 
-const initialCards = [
+let initialCards = [
     {
       name: 'Москва',
       link: 'https://images.unsplash.com/photo-1561217680-8cd186af7701?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=334&q=80'
@@ -28,25 +29,43 @@ const initialCards = [
     }
   ];
 
-  let getCard =  function (item) {
+  function getCard (item) {
     const newCard = templateEl.content.cloneNode(true);
     const elImage = newCard.querySelector('.element__image');
     elImage.src = item.link;
     elImage.alt = item.name;
     const elTitle = newCard.querySelector('.element__title-text');
     elTitle.textContent = item.name;
+    
+    let buttonTrash = newCard.querySelector ('.element__trash');    
+    buttonTrash.addEventListener ('click', deleteCard);
 
-    return newCard;
+    let buttonLike = newCard.querySelector('.element__title-like');
+    buttonLike.addEventListener('click', cardLike);
+
+    function check (evt) {
+      let area = evt.target;
+      let checkArea = area.closest('.element');
+      if (evt.target === checkArea.querySelector('.element__image') || evt.target === checkArea.querySelector('.element') || evt.target === checkArea.querySelector('.element__title') || evt.target === checkArea.querySelector('.element__title-text')) {
+      popupViewWindow.classList.toggle('popup-view_active')
+      viewImage.src = item.link;
+      viewImage.alt = item.name;
+      viewTitle.textContent = item.name;
+    }
+    }
+    
+    elImage.addEventListener('click', check);
+
+    return newCard
 }
 
 function render () {
-    const html = initialCards
+    let html = initialCards
     .map(getCard);
-
     listContainerEl.append(...html);
 }
 
-render ();
+// попап из первого задания
 
 let buttonEditOpen = document.querySelector('.profile__edit-button');
 let buttonEditClose = document.querySelector('.popup-edit__action-close');
@@ -93,6 +112,8 @@ function formSubmitHandlerEdit (evt) {
 
 formElementEdit.addEventListener ('submit', formSubmitHandlerEdit)
 
+// попап из второго задания на добавление карточек
+
 let buttonAddOpen = document.querySelector('.profile__add-button');
 let buttonAddClose = document.querySelector('.popup-add__action-close');
 let popupAddWindow = document.querySelector('.popup-add');
@@ -113,27 +134,68 @@ if (evt.target === evt.currentTarget) {
 
 popupAddWindow.addEventListener ('click', popUpAddClose);
 
-// let infoTitle = document.querySelector('.profile__visitor-name');
-// let infoAttribute = document.querySelector('.profile__visitor-attribute');
+// переменные просмотрщика
 
-// let formElementEdit = document.querySelector('.popup-edit__action-window');
+let viewImage = document.querySelector('.popup-view__image');
+let viewTitle = document.querySelector('.popup-view__title');
 
-// let inputTitleEdit = formElement.querySelector('.popup-edit__profile_name');
-// let inputAttributeEdit = formElement.querySelector('.popup-edit__profile_attribute');
+// закрытие попапа просмотра карточек
 
-// let nameInput = "";
-// let jobInput = "";
+let popupViewWindow = document.querySelector('.popup-view');
+let popupViewClose = document.querySelector('.popup-view__action-close');
 
-// inputTitleEdit.value = infoTitle.textContent;
-// inputAttributeEdit.value = infoAttribute.textContent;
+let popUpViewFunc = function (evt) {
+  evt.preventDefault ();
+  popupViewWindow.classList.toggle('popup-view_active');
+}
 
-// function formSubmitHandlerEdit (evt) {
-//     evt.preventDefault();    
-//     nameInput = inputTitleEdit.value;
-//     jobInput = inputAttributeEdit.value;    
-//     infoTitle.textContent = nameInput;
-//     infoAttribute.textContent = jobInput;
-//     popupEditWindow.classList.toggle('popup-edit_active');
-// }
+popupViewClose.addEventListener ('click', popUpViewFunc);
 
-// formElementEdit.addEventListener ('submit', formSubmitHandlerEdit)
+let popUpViewClose = function (evt) {
+if (evt.target === evt.currentTarget) {
+ popUpViewFunc (evt)
+}
+}
+
+popupViewWindow.addEventListener ('click', popUpViewClose);
+
+// добавление карточки вручную
+
+const formElementAdd = popupAddWindow.querySelector('.popup-add__action-window');
+const inputAddName = formElementAdd.querySelector('.popup-add_name');
+const inputAddLink = formElementAdd.querySelector('.popup-add_link');
+
+function addCard (card) {
+    listContainerEl.prepend(card);
+} 
+
+function renderCard (evt) {
+    evt.preventDefault ();
+    addCard (getCard({name: inputAddName.value, link: inputAddLink.value}));
+    inputAddName.value = '';
+    inputAddLink.value = '';
+    popupAddWindow.classList.toggle('popup-add_active');
+}
+
+formElementAdd.addEventListener('submit', renderCard)
+
+// удаление карточки
+
+function deleteCard (evt) {
+    const targetEl = evt.target;
+    const targetItem = targetEl.closest('.element');
+    targetItem.remove();
+} 
+
+// кнопка лайк
+
+function cardLike (evt) {
+  let targetLike = evt.target;
+  let targetedLike = targetLike.closest('.element__title-like');
+  targetedLike.classList.toggle('element__title-like_set');
+}
+
+
+
+render ();
+// отрисовано
