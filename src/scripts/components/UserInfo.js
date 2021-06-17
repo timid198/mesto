7512557edd4eaa9bbc21ffc24562edaf0c0da605
Andrey Azovkin <adm.az-an@yandex.ru>
2021-06-17 16:07:33 +0300

    Update UserInfo.js

--- src/scripts/components/UserInfo.js
+++ src/scripts/components/UserInfo.js
@@ -3,7 +3,7 @@ export default class UserInfo {
     this._userName = document.querySelector(userNameSelector);
     this._userAbout = document.querySelector(userAboutSelector);
     this._userAvatar = document.querySelector(userAvatarSelector);
-    this._id = _id;
+    this._id = 0123;
   }
 
   getUserInfo() {
